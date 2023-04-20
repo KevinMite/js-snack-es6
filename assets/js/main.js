87cf89ead70c1639d9ecb755c84fbb390599daf6
KevinMite <kr.mite96@gmail.com>
2023-04-20 18:15:00 +0200

    Snack 2 completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,14 +36,55 @@ document.writeln(`La bici più leggera è ${ name }, il suo peso è ${ peso }`)
  
 
 
-
-
 // Snack2
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà:
 // Punti fatti e falli subiti.
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const teams = [{
+    nome: 'PSG',
+    punti_fatti: 0,
+    falli_subiti: 0
+},
+
+{
+    nome: 'Ajax',
+    punti_fatti: 0,
+    falli_subiti: 0
+},
+
+{
+    nome: 'Manchester City',
+    punti_fatti: 0,
+    falli_subiti: 0
+},
+
+{
+    nome: 'Real Madrid',
+    punti_fatti: 0,
+    falli_subiti: 0
+}
+];
+
+function numRand(){
+    return Math.floor( Math.random() * 100) + 1
+}
+
+teams.forEach( (element) => {
+    element.punti_fatti = numRand ()
+    element.falli_subiti = numRand ()
+})
+
+console.log(teams)
+
+let newArray = teams.map( (nome, falli_subiti) => {
+    return {nome, falli_subiti}
+})
+console.log(newArray)
+
+
 
 // Snack 3
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
